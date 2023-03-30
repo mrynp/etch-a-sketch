c2e3434c247b43b64e9293891d6a2f240c1ceed6
Marienne Panganiban <marienneyen@yahoo.com>
2023-03-30 13:37:55 +0800

    Added a button that lights up when coloring

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 let size = getSize();
 let click = false;
 let color = "black";
+const board = document.querySelector(".board");
 
 createBoard(16);
 
 function createBoard(size) {
   resetBoard();
-
-  const board = document.querySelector(".board");
 
   board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
@@ -18,7 +17,7 @@ function createBoard(size) {
     let div = document.createElement("div");
     div.addEventListener("mouseover", colorDiv);
     board.insertAdjacentElement("beforeend", div);
-  }
+    }
 }
 
 
@@ -27,26 +26,59 @@ function getSize() {
   newSize.onclick = (e) => createBoard(e.target.value);
 }
 
+const active = document.querySelector(".active");
+
+
 function colorDiv() {
   if (click) {
     if (color == "random") {
-      this.style.backgroundColor = `hsl(${Math.random() * 360}, 85%, 48%)`;
+      this.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
     } else {
       this.style.backgroundColor = color;
     }
   }
 }
 
+
+
 function setColor(colorChoice) {
   color = colorChoice;
 }
 
 function resetBoard() {
-    let divs = document.querySelector(".board");
-    let square = divs.querySelectorAll("div");
+    let square = board.querySelectorAll("div");
     square.forEach((div) => div.style.backgroundColor = "white");
 }
 
-document.querySelector(".board").addEventListener("click", (e) => {
+board.addEventListener("click", () => {
   click = !click;
+  if (click) {
+    active.style.backgroundColor = "#86d00e";
+  } else {
+    active.style.backgroundColor = "#d0420e";
+  }
+
 });
+
+
+const rangeInputs = document.querySelectorAll('input[type="range"]');
+const numberInput = document.querySelector('input[type="number"]');
+
+function handleInputChange(e) {
+  let target = e.target
+  if (e.target.type !== 'range') {
+    target = document.getElementById('range');
+  } 
+  const min = target.min;
+  const max = target.max;
+  const val = target.value;
+  
+  target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%';
+}
+
+rangeInputs.forEach(input => {
+  input.addEventListener('input', handleInputChange);
+})
+
+numberInput.addEventListener('input', handleInputChange);
+
