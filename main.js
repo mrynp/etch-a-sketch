0ec2668e56ec858aa311973f0e5476dff379f43f
mrynp <110476086+mrynp@users.noreply.github.com>
2024-08-19 12:23:55 +0800

    Add greyscale effect

--- main.js
+++ main.js
@@ -17,9 +17,8 @@ function createBoard(size) {
     let div = document.createElement("div");
     div.addEventListener("mouseover", colorDiv);
     board.insertAdjacentElement("beforeend", div);
-    }
+  }
 }
-
 
 function getSize() {
   let newSize = document.getElementById("size");
@@ -28,26 +27,36 @@ function getSize() {
 
 const active = document.querySelector(".active");
 
-
 function colorDiv() {
   if (click) {
     if (color == "random") {
-      this.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+      this.style.backgroundColor = `rgb(${Math.floor(
+        Math.random() * 256
+      )}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
+        Math.random() * 256
+      )})`;
+    } else if (color == "gray") {
+      if (this.style.backgroundColor.match(/rgba/)) {
+        let currentOpacity = Number(this.style.backgroundColor.slice(-4, -1));
+        if (currentOpacity <= 0.9) {
+          this.style.backgroundColor = `rgba(0, 0, 0, ${currentOpacity + 0.1})`;
+        }
+      } else {
+        this.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
+      }
     } else {
       this.style.backgroundColor = color;
     }
   }
 }
 
-
-
 function setColor(colorChoice) {
   color = colorChoice;
 }
 
 function resetBoard() {
-    let square = board.querySelectorAll("div");
-    square.forEach((div) => div.style.backgroundColor = "white");
+  let square = board.querySelectorAll("div");
+  square.forEach((div) => (div.style.backgroundColor = "white"));
 }
 
 board.addEventListener("click", () => {
@@ -57,28 +66,22 @@ board.addEventListener("click", () => {
   } else {
     active.style.backgroundColor = "#d0420e";
   }
-
 });
 
-
 const rangeInputs = document.querySelectorAll('input[type="range"]');
-const numberInput = document.querySelector('input[type="number"]');
 
 function handleInputChange(e) {
-  let target = e.target
-  if (e.target.type !== 'range') {
-    target = document.getElementById('range');
-  } 
+  let target = e.target;
+  if (e.target.type !== "range") {
+    target = document.getElementById("range");
+  }
   const min = target.min;
   const max = target.max;
   const val = target.value;
-  
-  target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%';
+
+  target.style.backgroundSize = ((val - min) * 100) / (max - min) + "% 100%";
 }
 
-rangeInputs.forEach(input => {
-  input.addEventListener('input', handleInputChange);
-})
-
-numberInput.addEventListener('input', handleInputChange);
-
+rangeInputs.forEach((input) => {
+  input.addEventListener("input", handleInputChange);
+});
